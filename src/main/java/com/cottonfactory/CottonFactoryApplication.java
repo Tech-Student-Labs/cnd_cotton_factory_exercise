207af05750cdba18f7b58a52994ce8ca3d91e57f
K Hudson <kay.hudson@galvanize.com>
2021-01-24 02:09:10 -0800

    Add swagger docs.

--- src/main/java/com/cottonfactory/CottonFactoryApplication.java
+++ src/main/java/com/cottonfactory/CottonFactoryApplication.java
@@ -2,10 +2,33 @@ package com.cottonfactory;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@Configuration
+@EnableSwagger2
 public class CottonFactoryApplication {
     public static void main(String[] args) {
         SpringApplication.run(CottonFactoryApplication.class, args);
+    }
+
+    @Bean
+    public Docket productApi() {
+        return new Docket(DocumentationType.SWAGGER_2).select()
+                .apis(RequestHandlerSelectors.basePackage("com.cottonfactory")).build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder().title("Cotton Factory API Docs")
+                .description("Cotton Factory developer API documentation")
+                .license("GPLv3")
+                .licenseUrl("https://www.gnu.org/licenses/gpl-3.0.en.html").version("1.0").build();
     }
 }
---
+++ src/main/java/com/cottonfactory/ProductApiHome.java
@@ -0,0 +1,15 @@
+package com.cottonfactory;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/products")
+public class ProductApiHome {
+
+    @GetMapping
+    public String welcomeToDocs() {
+        return "Welcome home, Swagger!";
+    }
+}
