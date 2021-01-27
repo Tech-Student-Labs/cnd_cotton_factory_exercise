c0e5aa82c8166d63abbbb853cfa631ce0bf8fe98
Ofeoritse nanna <nannaofe20@gmail.com>
2021-01-26 18:30:12 -0600

    added the service layer and  mock testing for createTie

---
+++ src/main/java/com/cottonfactory/products/services/TieService.java
@@ -0,0 +1,11 @@
+package com.cottonfactory.products.services;
+
+import com.cottonfactory.products.entities.Tie;
+
+public class TieService {
+
+
+    public Tie createTie(Tie tie) {
+        return new Tie();
+    }
+}
