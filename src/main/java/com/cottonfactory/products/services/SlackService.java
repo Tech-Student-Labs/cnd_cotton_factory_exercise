13e3956780b919744f0002a8ef13ecb29871028c
M_788192 <m_788192@ambus54920.home>
2021-01-26 18:33:47 -0600

    getAllSlacks created with tests using mocked service

---
+++ src/main/java/com/cottonfactory/products/services/SlackService.java
@@ -0,0 +1,12 @@
+package com.cottonfactory.products.services;
+
+import com.cottonfactory.products.entities.Slack;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SlackService {
+    public List<Slack> findAll() {
+        return new ArrayList<>();
+    }
+}
---
+++ src/test/java/com/cottonfactory/products/controllers/SlackControllerTest.java
@@ -0,0 +1,52 @@
+package com.cottonfactory.products.controllers;
+
+import com.cottonfactory.products.entities.Slack;
+import com.cottonfactory.products.services.SlackService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest
+public class SlackControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private SlackService slackService;
+// /api/products/slacks
+    @Test
+    public void test_getAllSlacks_return200_withEmptyBody_whenNoslackFound() throws Exception {
+        when(slackService.findAll()).thenReturn(new ArrayList<>());
+        mockMvc.perform(get("/api/products/slacks"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.length()").value(0));
+    }
+
+    @Test
+    public void test_getAllSlacks_return200_withDetailFound_excludingWaistInseam() throws Exception {
+        Slack expectedSlack = Slack
+                .builder()
+                .type("formal")
+                .build();
+        when(slackService.findAll()).thenReturn(Arrays.asList(expectedSlack));
+
+        mockMvc.perform(get("/api/products/slacks"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$").isNotEmpty())
+                .andExpect(jsonPath("$[0]").value(expectedSlack))
+                .andExpect(jsonPath("$[0].inseam").doesNotExist())
+                .andExpect(jsonPath("$[0].waist").doesNotExist());
+    }
+
+}
