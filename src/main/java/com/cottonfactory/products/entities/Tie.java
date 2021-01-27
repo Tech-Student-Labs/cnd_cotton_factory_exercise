3d44a3145c6cd1d98e8304d9c84ecb9ca7698794
Ofeoritse nanna <nannaofe20@gmail.com>
2021-01-26 18:17:07 -0600

    implemented the unit test for adding a tie

---
+++ src/main/java/com/cottonfactory/products/entities/Tie.java
@@ -0,0 +1,86 @@
+package com.cottonfactory.products.entities;
+
+import java.math.BigDecimal;
+
+public class Tie {
+
+  private Integer id;
+  private String  type;
+  private Integer length;
+  private Integer width;
+  private String material;
+  private String designer;
+  private String color;
+  private BigDecimal price;
+
+    public Tie() {
+    }
+
+    public Tie(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Integer getLength() {
+        return length;
+    }
+
+    public void setLength(Integer length) {
+        this.length = length;
+    }
+
+    public Integer getWidth() {
+        return width;
+    }
+
+    public void setWidth(Integer width) {
+        this.width = width;
+    }
+
+    public String getMaterial() {
+        return material;
+    }
+
+    public void setMaterial(String material) {
+        this.material = material;
+    }
+
+    public String getDesigner() {
+        return designer;
+    }
+
+    public void setDesigner(String designer) {
+        this.designer = designer;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+}
