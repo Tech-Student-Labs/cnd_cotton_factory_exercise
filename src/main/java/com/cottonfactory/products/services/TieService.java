3c77ae33de81e37207702f6927f1d885ae01a9b2
Ofeoritse nanna <nannaofe20@gmail.com>
2021-01-26 18:45:24 -0600

    implemented unit test for service layer for createTie

--- src/main/java/com/cottonfactory/products/services/TieService.java
+++ src/main/java/com/cottonfactory/products/services/TieService.java
@@ -1,11 +1,14 @@
 package com.cottonfactory.products.services;
 
 import com.cottonfactory.products.entities.Tie;
+import org.springframework.stereotype.Service;
 
+@Service
 public class TieService {
 
 
     public Tie createTie(Tie tie) {
-        return new Tie();
+        tie.setId(1);
+        return tie;
     }
 }
