24af99499a89a6ef17b968dad9c1a65374f00130
shashi <shashi2k6@gmail.com>
2021-01-28 18:44:22 -0500

    Get all dress with value.

--- src/main/java/com/cottonfactory/products/controllers/DressController.java
+++ src/main/java/com/cottonfactory/products/controllers/DressController.java
@@ -2,6 +2,7 @@ package com.cottonfactory.products.controllers;
 
 import com.cottonfactory.products.entities.DressEntity;
 import com.cottonfactory.products.services.DressService;
+import io.swagger.annotations.Api;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -9,6 +10,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/products")
+@Api(tags = {"dress"})
 public class DressController {
 
     @Autowired
