3df14f1e91814df031655fc200c04a3873d4234d
NagaVijaya <nagavijayapatibandla@gmail.com>
2021-01-28 18:06:20 -0500

    added swagger api

--- src/main/java/com/cottonfactory/products/controllers/ShoeController.java
+++ src/main/java/com/cottonfactory/products/controllers/ShoeController.java
@@ -2,6 +2,7 @@ package com.cottonfactory.products.controllers;
 
 import com.cottonfactory.products.entities.ShoeProduct;
 import com.cottonfactory.products.services.ShoeService;
+import io.swagger.annotations.Api;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -11,6 +12,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/products/shoe")
+@Api(tags = "shoes")
 public class ShoeController {
 
     @Autowired
@@ -24,7 +26,6 @@ public class ShoeController {
 
     @GetMapping
     public List<ShoeProduct> getAllShoes(){
-
         return shoeService.getAllShoes();
     }
 }
