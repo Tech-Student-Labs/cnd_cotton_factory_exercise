208d262fd20f87f8f8b881256438d9d6da6a4b15
M_788192 <m_788192@ambus54920.home>
2021-01-28 17:11:58 -0600

    fixed tests

--- src/main/java/com/cottonfactory/products/services/SlackService.java
+++ src/main/java/com/cottonfactory/products/services/SlackService.java
@@ -1,10 +1,12 @@
 package com.cottonfactory.products.services;
 
 import com.cottonfactory.products.entities.Slack;
+import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Service
 public class SlackService {
     public List<Slack> findAllSlacks() {
         return new ArrayList<>();
--- src/test/java/com/cottonfactory/products/controllers/SlackControllerTest.java
+++ src/test/java/com/cottonfactory/products/controllers/SlackControllerTest.java
@@ -16,7 +16,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@WebMvcTest
+@WebMvcTest(SlackController.class)
 public class SlackControllerTest {
 
     @Autowired
